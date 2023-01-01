a130c646b7f386dbaa880a56b523eae3e56eef3a
OverbearingDust <2479942257@qq.com>
2023-01-01 19:10:42 +0800

    优化车牌定位，分割图像，提取车牌

--- function.py
+++ function.py
@@ -2,56 +2,119 @@ import cv2
 import numpy as np
 
 
-# 实现图片的定位
-def getPostion(img):
+# 拉伸图像
+def stretch(img):
+    Max = float(img.max())
+    Min = float(img.min())
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            img[i, j] = (255 / (Max - Min) * img[i, j] - (255 * Min) / (Max - Min))
+
+    return img
+
+
+# 图像二值化
+def toBinarization(img):
+    Max = float(img.max())
+    Min = float(img.min())
+
+    x = Max - ((Max - Min) / 2)
+    # 二值化,返回阈值ret  和  二值化操作后的图像thresh
+    ret, thresh = cv2.threshold(img, x, 255, cv2.THRESH_BINARY)
+    # 返回二值化后的黑白图像
+    return thresh
+
+
+# 获取轮廓左上角和右下角的坐标
+def getRect(contour):
+    y, x = [], []
+
+    for p in contour:
+        y.append(p[0][0])
+        x.append(p[0][1])
+
+    return [min(y), min(x), max(y), max(x)]
+
+
+# 定位车牌位置
+def locateLicense(img, img0):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # 找出最大的三个区域
+    block = []
+    for c in contours:
+        # 计算面积和长度比，
+        r = getRect(c)
+        s = (r[2] - r[0]) * (r[3] - r[1])  # 面积
+        l = (r[2] - r[0]) * (r[3] - r[1])  # 长度比
+
+        block.append([r, s, l])
+    # 选出面积最大的3个区域
+    block = sorted(block, key=lambda b: b[1])[-3:]
+
+    # 颜色识别
+    maxWeight, maxIndex = 0, -1
+    for i in range(len(block)):
+        b = img0[block[i][0][1]:block[i][0][3], block[i][0][0]:block[i][0][2]]
+
+        hsv = cv2.cvtColor(b, cv2.COLOR_BGR2HSV)
+        # 车牌的范围
+        lower = np.array([100, 50, 50])
+        upper = np.array([140, 255, 255])
+        # 根据阈值构建掩膜
+        mask = cv2.inRange(hsv, lower, upper)
+        # 统计权值
+        w1 = 0
+        for m in mask:
+            w1 += m / 255
+
+        w2 = 0
+        for n in w1:
+            w2 += n
+
+        # 选出最大权值的区域
+        if w2 > maxWeight:
+            maxIndex = i
+            maxWeight = w2
+
+    return block[maxIndex][0]
+
+
+# 预处理图像，并返回车牌位置
+def getPosition(img):
+    # 压缩图像
+    m = 400 * img.shape[0] / img.shape[1]
+    img = cv2.resize(img, (400, int(m)), interpolation=cv2.INTER_CUBIC)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_copy = img_gray.copy()
-    # 高斯滤波 将噪声
-    gray_img = cv2.GaussianBlur(img_copy, (5, 5), 0, 0, cv2.BORDER_DEFAULT)
-    # 开运算 将图像中不同部分分开
-    kernel = np.ones((23, 23), np.uint8)
-    img_opening = cv2.morphologyEx(gray_img, cv2.MORPH_OPEN, kernel)
-    img_opening = cv2.addWeighted(gray_img, 1, img_opening, -1, 0)  # 开运算后的图片与原始的灰度图片按一定权重进行一个融合
-    # 找到图像边缘
-    ret, img_thresh = cv2.threshold(img_opening, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)  # 阈值分割
-    img_edge = cv2.Canny(img_thresh, 100, 200)  # canny 边缘检测
-    # # 使用开运算和闭运算让图像边缘成为一个整体
-    kernel = np.ones((9, 9), np.uint8)
-    img_edge2 = cv2.morphologyEx(img_edge, cv2.MORPH_CLOSE, kernel)
-    # img_edge2 = cv2.morphologyEx(img_edge1, cv2.MORPH_OPEN, kernel) # 我测试时发现有这句代码，开运算就会把中文那部分去除
-    # # 查找图像边缘整体形成的矩形区域，可能有很多，车牌就在其中一个矩形区域中
-    contours, hierarchy = cv2.findContours(img_edge2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # 从找到的轮廓中找出车牌的位置
-    temp_contours = []
-    areaAverage = []
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        # print("面积 ", area)
-        areaAverage.append(area)
-    minArea = np.mean(areaAverage)
-    # print("平均面积", minArea)  #我们假设车牌的面积一定大于平均面积
-    temp_contours = []  # 先筛除较小的轮廓
-    for contour in contours:
-        if cv2.contourArea(contour) > minArea:
-            temp_contours.append(contour)
-    car_plates = []  # 可能的车牌
-    for temp_contour in temp_contours:
-        rect = cv2.minAreaRect(temp_contour)  # 该函数返回一个rect对象， rect[0]--中心点  rect[1] -- 矩形的长、宽
-        rect_width, rect_height = rect[1]
-        if rect_width < rect_height:  # 调整长宽
-            rect_width, rect_height = rect_height, rect_width
-        aspect_ratio = rect_width / rect_height
-        # 车牌正常情况下宽高比在2 - 5.5之间
-        if 2 < aspect_ratio < 5.5:
-            car_plates.append(temp_contour)
-    # 一车只有一个车牌
-    if len(car_plates) == 1:
-        for car_plate in car_plates:
-            row_min, col_min = np.min(car_plate[:, 0, :], axis=0)  # 左上角的坐标
-            row_max, col_max = np.max(car_plate[:, 0, :], axis=0)  # 右下角的坐标
-            # cv2.rectangle(img, (row_min, col_min), (row_max, col_max), (0, 255, 0), 2) # 在原图中画矩形框
-            card_img = img[col_min:col_max, row_min:row_max, :]  # 把矩形框中的图像显示出来
-            return card_img
-    else:
-        print("查找车牌失败")
-        return None
+
+    img_stretch = stretch(img_gray)  # 拉伸图像
+
+    # 开运算
+    r = 16
+    h = w = r * 2 + 1
+    kernel = np.zeros((h, w), np.uint8)
+    cv2.circle(kernel, (r, r), r, 1, -1)
+    img_open = cv2.morphologyEx(img_stretch, cv2.MORPH_OPEN, kernel)
+
+    img_temp = cv2.absdiff(img_stretch, img_open)
+
+    # 图像二值化
+    img_binary = toBinarization(img_temp)
+
+    # 边缘检测
+    canny = cv2.Canny(img_binary, img_binary.shape[0], img_binary.shape[1])
+
+    # 闭运算
+    kernel = np.ones((5, 19), np.uint8)
+    img_close = cv2.morphologyEx(canny, cv2.MORPH_CLOSE, kernel)
+
+    # 开运算
+    img_open = cv2.morphologyEx(img_close, cv2.MORPH_OPEN, kernel)
+    kernel = np.ones((11, 5), np.uint8)
+    img_open = cv2.morphologyEx(img_open, cv2.MORPH_OPEN, kernel)
+
+    # 定位车牌位置，获取矩形
+    rect = locateLicense(img_open, img)
+
+    return rect, img
