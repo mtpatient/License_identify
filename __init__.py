b48a1085d7ccba2df2b4d45bca2f1c1c0972ba4e
OverbearingDust <2479942257@qq.com>
2023-01-04 12:57:55 +0800

    计算运行时间并显示识别结果

--- __init__.py
+++ __init__.py
@@ -4,25 +4,42 @@ from PIL import Image, ImageTk
 import matplotlib.pyplot as plt
 import cv2
 import function
+import time
 
 
-# 导入图片
-def load_img():
+# 导入图片 图片路径不能包含中文
+def loadImg():
     file_path = filedialog.askopenfilename(title=u'选择图片')
-    print("图片路径：")
-    print(file_path)
+    # print("图片路径：" + file_path)
 
     img_open = Image.open(file_path)
-    global img
+    global img  # 全局变量声明
     img = ImageTk.PhotoImage(img_open)
 
     Label_img["image"] = img
-
     car_img = cv2.imread(file_path)
-    carLicense = function.getPostion(car_img)
 
-    plt.imshow(carLicense)
-    plt.show()
+    time_start = time.time()
+
+    global carLicense, text  # 全局变量声明
+    carLicense, text = function.getRes(car_img)
+
+    # plt.imshow(carLicense)
+    # plt.show()
+
+    carLicense = Image.fromarray(carLicense)
+    carLicense = ImageTk.PhotoImage(carLicense)
+
+    label0['image'] = carLicense
+    label0.pack()
+
+    label1['text'] = text
+    label1.pack()
+
+    time_end = time.time()
+
+    label2['text'] = str(time_end - time_start) + '  秒'
+    label2.pack()
 
 
 # 创建GUI窗口
@@ -35,7 +52,7 @@ left_frame = Frame(root, height=500, width=460)
 left_frame.pack(side='left')
 
 # 上传图片按钮
-btn_img = Button(left_frame, text="上传图片", height=1, width=10, command=lambda: load_img())
+btn_img = Button(left_frame, text="导入图片", height=1, width=10, command=lambda: loadImg())
 btn_img.place(x=0, y=0)
 
 Label_img = Label(left_frame, bg='Gainsboro', height=420, width=460)
@@ -47,6 +64,33 @@ right_frame.pack(side='right')
 
 Label_res_title = Label(right_frame, text='识别结果:', font=("微软雅黑", 12))
 Label_res_title.place(x=0, y=0)
+
+# 显示车牌
+Label_res_img_text = Label(right_frame, text='车牌位置：', font=("微软雅黑", 10))
+Label_res_img_text.place(x=1, y=30)
+
+Frame_res_img = Frame(right_frame, height=80, width=300)
+Frame_res_img.place(x=0, y=54)
+
+label0 = Label(Frame_res_img, height=40, width=300, bg='Gainsboro')
+
+# 识别结果
+Label_res_text_position = Label(right_frame, text='识别结果：', font=("微软雅黑", 10))
+Label_res_text_position.place(x=1, y=135)
+
+Frame_res_text = Frame(right_frame, height=80, width=300)
+Frame_res_text.place(x=0, y=164)
+
+label1 = Label(Frame_res_text, font=("微软雅黑", 16), justify='center', padx=100, bg='Gainsboro')
+
+# 运行时间
+Label_time = Label(right_frame, text='运行时间:', font=("微软雅黑", 10))
+Label_time.place(x=1, y=260)
+
+Frame_time_text = Frame(right_frame, height=40, width=300)
+Frame_time_text.place(x=0, y=290)
+
+label2 = Label(Frame_time_text, font=("微软雅黑", 16), justify='left', bg='Gainsboro')
 
 # 进入事件循环
 root.mainloop()
--- function.py
+++ function.py
@@ -1,5 +1,23 @@
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
+import pytesseract
+from cnocr import CnOcr
+import re
+
+
+# 显示彩色图片
+def plt_show_rgb(img):
+    b, g, r = cv2.split(img)
+    img = cv2.merge([r, g, b])
+    plt.imshow(img)
+    plt.show()
+
+
+# 显示灰度图
+def plt_show_gray(img):
+    plt.imshow(img, cmap='gray')
+    plt.show()
 
 
 # 拉伸图像
@@ -39,6 +57,7 @@ def getRect(contour):
 
 # 定位车牌位置
 def locateLicense(img, img0):
+    # 画出轮廓
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # 找出最大的三个区域
@@ -81,7 +100,7 @@ def locateLicense(img, img0):
     return block[maxIndex][0]
 
 
-# 预处理图像，并返回车牌位置
+# 预处理图像，并根据预处理好的图像返回车牌位置
 def getPosition(img):
     # 压缩图像
     m = 400 * img.shape[0] / img.shape[1]
@@ -118,3 +137,129 @@ def getPosition(img):
     rect = locateLicense(img_open, img)
 
     return rect, img
+
+
+# 识别车牌
+def getText(img):
+    ocr = CnOcr()  # 获取ocr对象
+    # t = ocr.ocr_for_single_line(img)
+    # print(t)
+    image = cv2.GaussianBlur(img, (3, 3), 0)  # 高斯去噪
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # plt_show_gray(gray_image)
+
+    # 自适应阈值处理
+    ret, image = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)
+    # image = toBinarization(image)
+    image = cv2.bitwise_not(image)
+
+    kernel = np.ones((1, 1), dtype=np.uint8)
+    # image = cv2.erode(image, kernel, iterations=1)  # 腐蚀
+    image = cv2.dilate(image, kernel, iterations=1)  # 膨胀
+
+    # plt_show_gray(image)
+
+    text = ocr.ocr_for_single_line(image)
+    # text = ocr.ocr(image)
+
+    '''
+    分割字符识别
+    '''
+    # words = splitLicense(img)
+    # text = pytesseract.image_to_string(image, lang="chi_sim+eng", config="--psm 7")
+
+    # text = []
+    # for w in words:
+    #     # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+    #     # w0 = cv2.dilate(w, kernel)
+    #     # plt_show_gray(w0)
+    #     t = pytesseract.image_to_string(w,
+    #                                     config=f'-l chi_sim')  # 识别文字
+    #     text.append(t)
+
+    # print(text)
+    res = text['text'].upper()
+    if res[2].isalpha():
+        res = res[:2] + '-' + res[2:]
+    else:
+        res = res[:2] + '-' + res[3:]
+
+    print(res)
+
+    return res
+
+
+# 分割字符
+def splitLicense(img):
+    image = cv2.GaussianBlur(img, (3, 3), 0)  # 高斯去噪
+    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+    # 自适应阈值处理
+    ret, image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_OTSU)
+    img1 = image.copy()
+    image2 = cv2.bitwise_not(image)
+    # text = pytesseract.image_to_string(image2,
+    #                                    config=f'--psm 8 chi_sim --oem 3 -c '
+    #                                           f'tessedit_char_whitelist=鄂赣甘贵桂黑沪冀津京吉辽鲁蒙闽宁青琼陕苏晋皖湘新豫渝粤云藏浙'
+    #                                           f'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+    text = pytesseract.image_to_string(image2, lang="chi_sim+eng", config="--psm 6")
+    print(text)
+    plt_show_gray(image2)
+
+    # 闭运算
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 4))
+    # 白色部分膨胀
+    image = cv2.dilate(image, kernel)
+    # plt_show_gray(image)
+
+    # 轮廓检测
+    contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    img0 = img.copy()
+    cv2.drawContours(img0, contours, -1, (0, 255, 0), 2)
+    plt.imshow(img0)
+    plt.show()
+
+    # 筛选出各个字符位置的轮廓
+    words = []
+    for i in contours:
+        word = []
+        rect = cv2.boundingRect(i)
+        x = rect[0]
+        y = rect[1]
+        width = rect[2]
+        height = rect[3]
+        word.append(x)
+        word.append(y)
+        word.append(width)
+        word.append(height)
+        words.append(word)
+
+    words = sorted(words, key=lambda s: s[0], reverse=False)
+
+    res = []
+    cnt = 0
+    for w in words:
+        if (w[3] > (w[2] > 1.8)) and (w[3] < (w[2] * 2.5)):
+            cnt += 1
+            image0 = img1[w[1]:w[1] + w[3], w[0]:w[0] + w[2]]
+            image0 = cv2.bitwise_not(image0)
+            res.append(image0)
+            # plt_show_gray(image0)
+
+    return res
+
+
+# 调用其它方法识别车牌，返回识别结果
+def getRes(img):
+    # 获取图像位置，并压缩图像
+    rect, img = getPosition(img)
+    x1, y1, x2, y2 = int(rect[0]), int(rect[1]), int(rect[2]), int(rect[3])
+
+    # 截取车牌
+    res_img = img[y1:y2, x1:x2]
+    res_img = cv2.cvtColor(res_img, cv2.COLOR_BGR2RGB)
+
+    # 获取文本
+    res_text = getText(res_img)
+
+    return res_img, res_text
